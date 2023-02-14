914f49c9ac7455f7698c472b464ab5ba4d6751f5
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2023-02-15 00:07:46 +0530

    making navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,10 +7,14 @@ const Navbar = () => {
             <img src="/RoveLogo.svg" className="w-44" alt="" />
 
         </div>
-        <div >
+        <div className="sm:hidden" >
             <button><i className="fa-solid fa-bars fa-2x"></i></button>
         </div>
-
+        <div className="hidden sm:inline">
+            <div>
+            
+            </div>
+        </div>
     </nav>
     <div className="w-full h-[1px] bg-black"></div>
     
