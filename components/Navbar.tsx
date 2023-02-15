bb720bcaafc02686088116822a815ed3a8f63f6c
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2023-02-15 10:52:54 +0530

    Testing Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,28 @@
 
+
 const Navbar = () => {
   return (
     <>
-    <nav className="flex w-full pt-2 pb-2 pr-4 justify-between  items-center">
-        <div>
-            <img src="/RoveLogo.svg" className="w-44" alt="" />
+    <nav className="flex w-full p-1 justify-center items-center">
+    <div className="w-[98vw] md:w-[94vw] md:mt-1 bg-[#1e1e1e] rounded-2xl flex p-2 justify-between  items-center">
+          <div className="md:ml-5">
+            <img src="/RoveLogoWhite.svg" className="w-44 md:w-[14rem]" alt="" />
 
         </div>
-        <div className="sm:hidden" >
-            <button><i className="fa-solid fa-bars fa-2x"></i></button>
+        <div className="sm:hidden pr-1" >
+            <button><i className="fa-solid fa-bars fa-2x text-[#f7f7f7]"></i></button>
         </div>
         <div className="hidden sm:inline">
-            <div>
-            
+            <ul className="flex mr-16">   
+            <li className="text-white pr-5  font-['Poppins'] antialiased font-bold text-lg"><a href={"/about"}>About</a></li>
+            <li className="text-white pr-5 font-['Poppins'] antialiased font-bold text-lg"><a href={"/about"}>Courses</a></li>
+            <li className="text-white pr-5 font-['Poppins'] antialiased font-bold text-lg"><a href={"/about"}>Services</a></li>
+            <li className="text-white pr-5 font-['Poppins'] antialiased font-bold text-lg"><a href={"/#about"}>Contact</a></li>
+            </ul>
             </div>
         </div>
     </nav>
-    <div className="w-full h-[1px] bg-black"></div>
-    
+     
     </>
   )
 }
